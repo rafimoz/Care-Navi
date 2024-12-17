3920f429458f4914ff5fa92f28b9a02b2630b5f6
Rafi Mozumder <87600955+rafimoz@users.noreply.github.com>
2024-12-17 14:07:49 +0600

    Update server.js
    asa

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRouter from './routes/userRoute.js'
 
 // App configuration
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 connectDB()
 connectCloudinary()
 
