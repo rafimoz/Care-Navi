ef241a15c1716a1e204265ee911e6abe271cfd4d
Rafi Mozumder <rafimozumder@gmail.com>
2025-05-27 21:04:25 +0600

    Refactor: Remove .env files and clean up backend code
    This commit removes the `.env` files from the admin, backend, and frontend directories. The environment variables are likely being managed in a different way, such as through a cloud provider or a dedicated configuration management system.
    
    Additionally, this commit cleans up the backend `server.js` file by removing unnecessary comments and whitespace, improving code readability.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express  from 'express'
-// const mongoose = require('mongoose')
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
@@ -7,28 +6,21 @@ import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
 import doctorRouter from './routes/doctorRoute.js'
 import userRouter from './routes/userRoute.js'
-
 // App configuration
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB()
 connectCloudinary()
-
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-
 // api endpoint
 app.use('/api/admin', adminRouter)
-
 app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRouter)
-
 app.get('/', (req, res) => {
   res.send('Hello from backend API');
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
